1d6ccea60bcd0662bcaca62f4b2a3f8ec31e0fd6
kcsamm <kcsamm11@gmail.com>
2023-02-28 23:59:08 +0100

    Working on Undirected Path algorithm

--- graphs/02_undirected_path.go
+++ graphs/02_undirected_path.go
@@ -20,6 +20,7 @@ func undirectedPath(edges []string, nodeA string, nodeB string) bool {
 func hasPath(graph map[string][]string, src string, dst string, visited []string) bool {
     if src == dst { return true }
 	if ()
+	visited = append(visited, src)
 
 	for _, neighbor := range graph[src] {
 		if hasPath(graph, neighbor, dst) == true {
@@ -28,4 +29,35 @@ func hasPath(graph map[string][]string, src string, dst string, visited []string
 	}
 	return false
 }
+
+const hasPath = (graph, src, dst, visited) => {
+	if (src === dst) return true;
+	if (visited.has(src)) return false;
+
+	visited.add(src);
+
+	for (let neighbor of graph[src]) {
+		if (hasPath(graph, neighbor, dst, visited) === true) {
+			return true;
+		};
+	};
+
+	return false;
+}
+
+const buildEdges = (edges) => [
+	const graph = {};
+
+	for (let edge of edges) {
+		const [a, b] = edge;
+
+		if (!(a in graph)) graph[a] = [];
+		if (!(b in graph)) graph[b] = [];
+
+		graph[a].push(b);
+		graph[b].push(a);
+	}
+
+	return graph;
+]
 
