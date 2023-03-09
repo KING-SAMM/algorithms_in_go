ae139eb3368e2e1324e7c74e01f36f117cb733a6
kcsamm <kcsamm11@gmail.com>
2023-03-09 22:34:14 +0100

    Working on shortestPath

---
+++ graphs/05_shortest_path.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*--------------------------------------------
+| Shortest Path
+---------------------------------------------*/
+/**
+ * Write a function, shortestPath, that takes in an array of edges for an
+ * undirected graph and two nodes (nodeA, nodeB). The function should return the
+ * length of the shortest path between A and B. Consider the length as the number
+ * of edges in the path, not the number of nodes. If there is no path between A
+ * and B, then return -1.
+ * 
+ * @param {obj} graph 
+ * @param {String} nodeA 
+ * @param {String} nodeB 
+ */
